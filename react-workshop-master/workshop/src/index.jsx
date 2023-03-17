efb0d377864290e3efbfca8114d5f8cdb731f254
maxwell wong <midorikawamaxwell@gmail.com>
2023-03-17 10:39:05 +0900

    testing out jsx callig a function and rendering in root

--- react-workshop-master/workshop/src/index.jsx
+++ react-workshop-master/workshop/src/index.jsx
@@ -1,1 +1,14 @@
 import '../assets/stylesheets/application.scss';
+
+import React from 'react';
+import * as ReactDOM from 'react-dom';
+
+const Hello = (props) => {
+  const name = props.name;
+  const age = props.age;
+  return <h1>Hi {name}, your age is {age}!!!</h1>;
+};
+
+const containerDiv = document.querySelector('#root'); // selecting where you want the react to be
+const root = ReactDOM.createRoot(containerDiv); // creating root
+root.render(<Hello name="Max" age="23"/>); // rendering what you want in the root
